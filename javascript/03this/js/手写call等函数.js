0a7bfeab0595a3e5cd859f50f632b00d82c4c6a8
wengtianliang@163.com <wengtianliang@163.com>
2023-06-27 18:16:40 +0800

    this

--- javascript/03this/js/手写call等函数.js
+++ javascript/03this/js/手写call等函数.js
@@ -14,6 +14,17 @@ Function.prototype.myApply = function(context) {
   context.fn(arg)
   delete context.fn
 }
+// 手写bind
+Function.prototype.myBind = function(context) {
+  context = context || window
+  const arg = [...arguments].slice(1)
+  // console.log(arg)
+  let fn = this
+  return function(...args) {
+    return fn.myCall(context, ...arg.concat(args))
+  }
+}
+
 
 let obj = {
   name: '威威',
@@ -25,5 +36,9 @@ let obj1 = {
   name: '坤坤'
 }
 
-obj.showName.myCall(obj1, 1, 2)
-obj.showName.myApply(obj1, [1, 2])
+// obj.showName.myCall(obj1, 1, 2)
+// obj.showName.myApply(obj1, [1, 2])
+const fn = obj.showName.myBind(obj1, 1, 2, 3)
+// const fn = obj.showName.bind(obj1, 1, 2, 3)
+fn(1)
+fn(4, 5)
--- javascript/03this/readme.md
+++ javascript/03this/readme.md
@@ -1,3 +1,13 @@
 # this
-## this
+## this指向
+### this指向规则
+- 正常情况下，不同情况的调用，this指向不一样
+- 对于直接调用`foo`，this一定指向window；
+- 对于`obj.foo()`，谁调用函数，函数内this就指向谁；
+- 对于`new`方式，this永远绑定在new创建的实例上，不会被任何方式所改变this的指向；
+- 对于箭头函数而言，箭头函数中是没有this的，箭头函数中的this只取决于包裹箭头函数的第一个普通函数的this，且bind等方式对于箭头函数是无效的；
+- 最后也可以使用call、apply、bind方法来主动更改函数中this的指向；
+### this指向优先级
+
+
 ## 手写call、apply和bind
